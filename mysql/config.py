968d157cd9622fb8a166012966a742862d9ba34e
Lawrence Roble <lawrence7roble@gmail.com>
2025-06-09 16:35:47 +0800

    Fix bugs.

--- mysql/config.py
+++ mysql/config.py
@@ -1,6 +1,6 @@
 # config.py
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = '*$CTU4201400roble$*'
+MYSQL_PASSWORD = 'ChloeCutie'
 MYSQL_DB = 'attendance_system'
 SECRET_KEY = 'your_secret_key'
